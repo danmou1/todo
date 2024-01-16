a980190ac9d0fe5b7b53481565a0cc04d9e285c7
Danilo Moura <danilomou01@gmail.com>
2024-01-15 23:30:37 -0300

    initialized

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/task', (req, res) => {
+    res.send("GET /task");
+});
+
+app.post('/task', (req, res) => {
+    res.send("POST /task");
+});
+
+app.put('/task', (req, res) => {
+    res.send("PUT /task");
+});
+
+app.delete('/task', (req, res) => {
+    res.send("DELETE /task");
+});
