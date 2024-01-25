97ac9c79b1e3dc862667721289591fc2ffa8117d
Danilo Moura <danilomou01@gmail.com>
2024-01-25 19:14:49 -0300

    fixed href issue

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 
 const { initializeDatabase, runMigrations, closeDatabaseConnection } = require('./database');
 const { getTasks, createTask, updateTask, deleteTask } = require('./tasks');
@@ -8,9 +7,8 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 async function startServer() {
