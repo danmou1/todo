aca39a6e196441b735eb904132274b2a39225bc7
Danilo Moura <danilomou01@gmail.com>
2024-02-27 23:32:08 -0300

    added user creation query

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { getClient } = require('./connection');
 
 const client = getClient();
@@ -85,9 +86,29 @@ async function deleteTask(taskId) {
     await client.query('DELETE FROM tasks WHERE task_id = $1', [taskId]);
 }
 
+async function addUser(username, password) {
+    try {
+    const hash = crypto.createHash('sha256');
+    hash.update(password);
+
+    const hashedPassword = hash.digest('hex');
+
+    await client.query('INSERT INTO users (username, password_hash) VALUES ($1, $2)', [username, hashedPassword]);
+
+    console.log('User added successfully');
+    } catch (error) {
+        if (error.code === '23505') {
+            console.error('Error adding user: Username already exists');
+        } else {
+            console.error('Error adding user:', error);
+        }
+    }
+}
+
 module.exports = {
     getTasks,
     createTask,
     updateTask,
     deleteTask,
+    addUser,
 };
