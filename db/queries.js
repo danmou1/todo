2609140b061c554682c4b24e7ed914f68a8b4dbf
Danilo Moura <danilomou01@gmail.com>
2024-02-29 19:56:57 -0300

    fixes

--- db/queries.js
+++ db/queries.js
@@ -4,8 +4,6 @@ const { getClient } = require('./connection');
 const client = getClient();
 
 async function getTasks(options = {}) {
-    // todo: implement search by multiple parameters
-
     const {
         searchParams = null,
         isCompleted = null,
@@ -14,39 +12,43 @@ async function getTasks(options = {}) {
     } = options;
 
     let query = `SELECT * FROM tasks`;
-
     const values = [];
 
     if (searchParams) {
-        query += 'WHERE title ILIKE $1';
+        query += ' WHERE title ILIKE $1';
         values.push(`%${searchParams}%`);
     }
 
     if (isCompleted !== null) {
-        query +=  `WHERE completed IS ${isCompleted}`;
+        query += values.length ? ' AND' : ' WHERE'; 
+        query += ` completed IS ${isCompleted}`;
     }
 
     if (dueToday) {
-        query += `WHERE due_date = CURRENT_DATE`;
+        query += values.length ? ' AND' : ' WHERE';
+        query += ` due_date = CURRENT_DATE`;
     }
 
-    const result = await client.query(query, values);
-    return result.rows;
+    try {
+        const result = await client.query(query, values);
+        return result.rows;
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+    };
 }
 
 async function createTask(taskData) {
     const { title, description, dueDate, priority } = taskData;
 
-    try {
-        const result = await client.query(
-            'INSERT INTO tasks VALUES (DEFAULT, $1, $2, $3, CURRENT_TIMESTAMP, $4) RETURNING *',
-            [title, description, dueDate, priority]
-        );
-
-        return result.rows[0];
-    } catch (error) {
+    return client.query(
+        'INSERT INTO tasks VALUES (DEFAULT, $1, $2, $3, CURRENT_TIMESTAMP, $4) RETURNING *',
+        [title, description, dueDate, priority]
+    )
+    .then(result => result.rows[0])
+    .catch(error => {
         console.error('Error inserting task:', error);
-    }
+        return Promise.reject(error);
+    });
 }
 
 // overwrites all fields
@@ -63,22 +65,27 @@ async function updateTask(taskId, taskData) {
     params.push(taskId);
 
     const setClause = updates.join(', ');
-
-    try {
-        const result = await client.query(
-            `UPDATE tasks SET ${setClause} WHERE task_id = $${params.length} RETURNING *`,
-            params
-        );
-        
+    
+    return client.query(
+        `UPDATE tasks SET ${setClause} WHERE task_id = $${params.length} RETURNING *`,
+        params
+    )
+    .then(result => {
         console.log(`[${taskId}]: Updated task.`);
         return result.rows[0];
-    } catch (error) {
+    })  
+    .catch(error => {
         console.error('Error updating task:', error);
-    }
+        return Promise.reject(error);
+    });
 }
 
 async function deleteTask(taskId) {
-    await client.query('DELETE FROM tasks WHERE task_id = $1', [taskId]);
+    await client.query('DELETE FROM tasks WHERE task_id = $1', [taskId])
+    .catch(error => {
+        console.error('Error deleting task:', error);
+        return Promise.reject(error);
+    });
 }
 
 async function addUser(username, password) {
@@ -87,9 +94,14 @@ async function addUser(username, password) {
 
     const hashedPassword = hash.digest('hex');
 
-    await client.query('INSERT INTO users (username, password_hash) VALUES ($1, $2)', [username, hashedPassword]);
-
-    console.log('User added successfully');
+    return client.query('INSERT INTO users (username, password_hash) VALUES ($1, $2)', [username, hashedPassword])
+    .then(() => {
+        console.log('User added successfully');
+    })
+    .catch(error => {
+        console.error('Error adding user:', error);
+        return Promise.reject(error);
+    });
 }
 
 module.exports = {
