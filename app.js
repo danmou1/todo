2bb7e3f6cc20ea3a5a54b10fffe4a6349b220f1d
Danilo Moura <danilomou01@gmail.com>
2024-03-07 21:49:28 -0300

    added cookie-parser middleware

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const { initializeDatabase, runMigrations, closeDatabaseConnection } = require('
 const { getTasks, createTask, updateTask, deleteTask, addUser} = require('./db/queries');
 const { userAuth, verifyToken } = require('./db/userAuth')
 
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
+app.use(cookieParser());
 
 async function startServer() {
     try {
@@ -28,6 +30,14 @@ async function startServer() {
     }
 }
 
+app.use((req, res, next) => {
+    if (req.originalUrl === '/login' || req.originalUrl === '/register') {
+        return next();
+    }
+
+    verifyToken(req, res, next);
+});
+
 function setupRoutes() {
     app.get('/', (req, res) => {
         res.redirect('/login');
@@ -42,12 +52,14 @@ function setupRoutes() {
             const authResult = await userAuth(username, password);
 
             if (authResult) {
-                const { sessionToken } = authResult;
-                res.status(200).json({ sessionToken });
+                const { token } = authResult;
+
+                res.cookie('token', token, { maxAge: 1000*60*60, httpOnly: true});
+                res.status(200).json({ token });
             } else {
                 res.status(401).json({ error: 'Invalid credentials.' });
             }
-        })
+        });
 
     app.route('/register')
         .get((req, res) => {
@@ -57,13 +69,13 @@ function setupRoutes() {
             const { username, password } = req.body;
             addUser(username, password)
                 .then(() => res.status(200).send({ success: true }))
-        })
+        });
 
     app.route('/app/tasks')
         .get(async (req, res) => {
             const { q: searchParams, d: date, c: isCompleted } = req.query;
             const tasks = await getTasks(searchParams, date, isCompleted);
-            console.log(req.headers.authorization);
+
             res.render('layout', { pageTitle: 'Tasks', tasks });
         })
         .post(async (req, res) => {
