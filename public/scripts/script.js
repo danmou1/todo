6b640bf071000aa9d6a250bffb1f136d37bfe02b
Danilo Moura <danilomou01@gmail.com>
2024-03-13 19:56:44 -0300

    merged admin script with user script TEMPORARILY

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const newTaskButton = document.getElementById('new-task-button');
-    const formContainer = document.getElementById('task-form-container');
-    const tasksContainer = document.getElementById('tasks-container');
-    const searchForm = document.getElementById('search-form');
+const newTaskButton = document.getElementById('new-task-button');
+const taskFormContainer = document.getElementById('task-form-container');
+const tasksContainer = document.getElementById('tasks-container');
+const searchForm = document.getElementById('search-form');
+const taskForm = document.getElementById('task-form');
 
+document.addEventListener('DOMContentLoaded', function() {
     searchForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -34,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     newTaskButton.addEventListener('click', function () {
-        formContainer.style.display = 'block';
+        taskFormContainer.style.display = 'block';
         tasksContainer.style.display = 'none';
     });
 
@@ -90,6 +91,82 @@ async function deleteTask(taskId, title) {
         } else {
             const errorMessage = await response.text();
             console.error(`Error deleting task: ${errorMessage}`);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+function editTask(task_id) {
+    const taskRow = document.getElementById(`task-row-${task_id}`);
+    const titleInput = taskForm.elements['title'];
+    const descriptionInput = taskForm.elements['description'];
+    const dueDateInput = taskForm.elements['dueDate'];
+    const priorityInput = taskForm.elements['priority']
+    
+    titleInput.value = taskRow.cells[1].textContent;
+    descriptionInput.value = taskRow.cells[2].textContent;
+    dueDateInput.value = taskRow.cells[3].textContent;
+    priorityInput.value = taskRow.cells[4].textContent;
+
+    taskForm.method = 'PUT';
+
+    taskFormContainer.style.display = 'block';
+    tasksContainer.style.display = 'none';;
+}
+
+const newUserButton = document.getElementById('new-user-button');
+const usersContainer = document.getElementById('users-container');
+const userFormContainer = document.getElementById('user-form-container');
+const userForm = document.getElementById('user-form');
+
+newUserButton.addEventListener('click', function() {
+    userFormContainer.style.display = 'block';
+    usersContainer.style.display = 'none';
+});
+
+function editUser(userId) {
+    const userRow = document.getElementById(`user-row-${userId}`);
+    const uidInput = userForm.elements['userid'];
+    const usernameInput = userForm.elements['username'];
+    const roleInput = userForm.elements['role'];
+    
+    uidInput.value = userId;
+    usernameInput.value = userRow.cells[1].textContent;
+    roleInput.value = userRow.cells[2].textContent;;
+
+    userFormContainer.style.display = 'block';
+    usersContainer.style.display = 'none';;
+}
+    
+async function deleteUser(userId) {
+    console.log('Deletion started');
+
+    const confirmed = confirm(`Are you sure you want to delete user with ID: "${userId}"?`);
+    
+    if (!confirmed) {
+        return;
+    }
+
+    try {
+        const response = await fetch(`/app/users/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            const deletedRow = document.getElementById(`user-row-${userId}`);
+            if (deletedRow) {
+                deletedRow.remove();
+                console.log("User deleted successfully.");
+            } else {
+                console.error("Error: Deleted row not found in the table.");
+            }
+        } else {
+            const errorMessage = await response.text();
+            console.error(`Error deleting user: ${errorMessage}`);
         }
     } catch (error) {
         console.error("Error:", error);
