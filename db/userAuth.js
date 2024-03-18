d5130e3faa15c34286119ba1a25e164841042716
Danilo Moura <danilomou01@gmail.com>
2024-03-18 16:36:03 -0300

    added role to token

--- db/userAuth.js
+++ db/userAuth.js
@@ -16,34 +16,34 @@ function verifyPassword(password, hashedPassword, salt) {
 };
 
 //queries role in database then add the role to the user object.
-async function checkRole(req, res, next) {
-    const userId = req.user.userId;
 
-    try {
-        const result = await client.query('SELECT role FROM users WHERE user_id = $1', [userId]);
-        if (result.rows.length === 0) {
-            return res.status(403).json({ error: 'Forbidden' });
-        }
-        const role = result.rows[0].role;
-        req.user.role = role;
+// async function checkRole(req, res, next) {
+//     const userId = req.user.userId;
 
-        next();
-    } catch (err) {
-        console.error('Error querying the database:', err);
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-};
+//     try {
+//         const result = await client.query('SELECT role FROM users WHERE user_id = $1', [userId]);
+//         if (result.rows.length === 0) {
+//             return res.status(403).json({ error: 'Forbidden' });
+//         }
+//         const role = result.rows[0].role;
+//         req.user.role = role;
 
-function authRole(role) {
-    return (req, res, next) => {
+//         next();
+//     } catch (err) {
+//         console.error('Error querying the database:', err);
+//         return res.status(500).json({ error: 'Internal server error' });
+//     }
+// };
+
+function authRole(role, callback) {
+    return (req, res) => {
         if (req.user.role !== role) {
             res.status(401);
-            return res.send('Not Allowed');;
+            return res.send('Not Allowed');
         }
-
-        next();
+        callback(req, res);
     }
-}
+};
 
 async function verifyToken(req, res, next) {
     const token = req.cookies.token;
@@ -70,11 +70,11 @@ async function userAuth(username, password) {
         if (!user || !verifyPassword(password, user.password_hash, user.salt)) {
             return null;
         }    
-        const token = generateToken({ userId: user.user_id, username: user.username});
+        const token = generateToken({ userId: user.user_id, username: user.username, role: user.role });
 
         return { user, token }
     } catch (err) {
-        console.error('uAuth error:', error);
+        console.error('uAuth error:', err);
         throw err;
     }
 };
@@ -82,6 +82,6 @@ async function userAuth(username, password) {
 module.exports = {
     userAuth,
     verifyToken,
-    checkRole,
+    // checkRole,
     authRole,
 };
